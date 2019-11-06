133567b40c13f0d334e326093bd6b363b5e0a4c3
Lynn <lynn@foldr.moe>
2019-11-06 23:48:35 +0100

    1.2.0: Shift+J (Other Audio)

--- hotkeys.user.js
+++ hotkeys.user.js
@@ -2,7 +2,7 @@
 // @name          WaniKani More Hotkeys
 // @namespace     https://www.wanikani.com
 // @description   In kanji info, press 'S/M/R' to add synonym/meaning/reading note. Press 'W' to wrap up.
-// @version       1.1.0
+// @version       1.2.0
 // @include       *://www.wanikani.com/lesson/session*
 // @include       *://www.wanikani.com/review/session*
 // @grant         none
@@ -10,36 +10,72 @@
 
 (function() {
   'use strict';
-  var haveInfo = function()   { return $('#item-info h2').is(':visible') }
-  var waitInfo = function(cb) { haveInfo() ? cb() : setTimeout(function() { waitInfo(cb) }, 100) }
-  var withInfo = function(cb) { haveInfo() ? cb() : ($('#option-item-info').trigger('click'), waitInfo(cb)) }
-  var focusInputIn = function(selector) { var input = $(selector + ' textarea')[0] || $(selector + ' input')[0]; input.focus(); }
-  var editNote = function(selector, buttonSelector) {
-    buttonSelector = buttonSelector || selector;
-    withInfo(function() {
+
+  // Helper functions.
+  var toggleHotkeys = () => $('#hotkeys').trigger('click');
+  var wrapUp = () => $('#option-wrap-up').trigger('click');
+  var infoLoaded = () => $('#item-info h2').is(':visible');
+  var canGetInfo = () => !$('#option-item-info').is('.disabled');
+  var clickShowInfo = () => $('#option-item-info').trigger('click');
+  var clickShowAllInfo = () => $('#all-info').trigger('click');
+  var canClickAudioButton = () => !$('#option-audio').is('.disabled');
+  var clickAudioButton = () => $('#option-audio').trigger('click');
+
+  // Expand flashcard info until `selector` is visible, then call `cb`.
+  var withInfo = (selector, cb, clicked) => {
+    if (infoLoaded()) {
       if ($(selector).is(':visible')) {
-        $(buttonSelector).trigger('click');
-        setTimeout(function() { focusInputIn(selector) }, 50);
+        cb();
       } else {
-        $('#all-info').trigger('click');
-        setTimeout(function() { editNote(selector, buttonSelector) }, 1);
+        clickShowAllInfo();
+        setTimeout(cb, 1);
       }
+    } else if (canGetInfo()) {
+      if (!clicked) clickShowInfo();
+      setTimeout(() => withInfo(selector, cb, true), 100);
+    }
+  };
+
+  // Expand info until `selector` is visible. Then click `button` and focus `input`.
+  var clickEdit = (selector, button, input) => {
+    withInfo(selector, () => {
+      $(button).trigger('click');
+      setTimeout(() => $(input).focus(), 50);
     });
   }
-  var addSynonym = function() { editNote('.user-synonyms', '.user-synonyms-add-btn'); }
 
-  $(document).on('keydown.reviewScreen', function(e) {
+  var addSynonym = () => clickEdit('.user-synonyms', '.user-synonyms-add-btn', '.user-synonyms input');
+  var editMeaning = () => clickEdit('.note-meaning', '.note-meaning', '.note-meaning textarea');
+  var editReading = () => clickEdit('.note-reading', '.note-reading', '.note-meaning textarea');
+
+  // Play the preferred or non-preferred audio file.
+  // This will use the big audio button if it's available.
+  // Otherwise, it expands and uses the little item-info audio players.
+  var playAudio = (preferred) => {
+    if (preferred && canClickAudioButton()) {
+      clickAudioButton();
+    } else {
+      withInfo('.audio-player', () => {
+        var audio = $('.audio-player>audio').filter((i, e) => $(e).is('.preferred') === preferred)[0];
+        if (audio) audio.play();
+      });
+    }
+  };
+
+  $(document).on('keydown.reviewScreen', (e) => {
     // Don't fire hotkeys until the review UI is visible.
     if (!$('#reviews').is(':visible')) return;
 
     // The main hotkeys.
+    var shift = e.shiftKey;
     if (!$('input, textarea').is(':focus')) {
       switch (e.key.toLowerCase()) {
-        case '?': $('#hotkeys').click();        e.preventDefault(); break;
-        case 'w': $('#option-wrap-up').click(); e.preventDefault(); break;
-        case 's': addSynonym();                 e.preventDefault(); break;
-        case 'm': editNote('div.note-meaning'); e.preventDefault(); break;
-        case 'r': editNote('div.note-reading'); e.preventDefault(); break;
+        case '?': toggleHotkeys();   e.preventDefault(); break;
+        case 'w': wrapUp();          e.preventDefault(); break;
+        case 's': addSynonym();      e.preventDefault(); break;
+        case 'm': editMeaning();     e.preventDefault(); break;
+        case 'r': editReading();     e.preventDefault(); break;
+        case 'j': playAudio(!shift); e.preventDefault(); break;
         default: break;
       }
     }
@@ -75,12 +111,16 @@
 
   // Update hotkey list.
   $(document).ready(function() {
-    $('#hotkeys tr:nth-child(2)').after(
-      '<tr><td><span>S</span></td><td>Add Synonym</td></tr>' +
-      '<tr><td><span>M</span></td><td>Add Meaning Note</td></tr>' +
-      '<tr><td><span>R</span></td><td>Add Reading Note</td></tr>' +
-      '<tr><td><span>W</span></td><td>Wrap Up</td></tr>'
+    $('#hotkeys>table>tbody:last').after(
+      '<tbody style="color:purple;">' +
+      ' <tr><td colspan="2"><hr></td></tr>' +
+      ' <tr><td><span>S</span></td><td>Add Synonym</td></tr>' +
+      ' <tr><td><span>M</span></td><td>Add Meaning Note</td></tr>' +
+      ' <tr><td><span>R</span></td><td>Add Reading Note</td></tr>' +
+      ' <tr><td><span>W</span></td><td>Wrap Up</td></tr>' +
+      ' <tr><td><span>Shift+J</span></td><td>Other Audio</td></tr>' +
+      ' <tr><td><span>?</span></td><td>Show Hotkeys</td></tr>' +
+      '</tbody>'
     );
-    $('#hotkeys tr:last').after('<tr><td><span>?</span></td><td>Show Hotkeys</td></tr>');
   });
 })();
