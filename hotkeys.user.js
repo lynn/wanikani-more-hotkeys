ff5995ef370308c31f29cc1b9304f7b6bda5d78a
Lynn <lynn@foldr.moe>
2019-11-12 15:40:38 +0100

    1.3.0: Lesson hotkeys

--- hotkeys.user.js
+++ hotkeys.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name          WaniKani More Hotkeys
 // @namespace     https://www.wanikani.com
-// @description   In kanji info, press 'S/M/R' to add synonym/meaning/reading note. Press 'W' to wrap up.
-// @version       1.2.0
+// @description   Press 'S/M/R' to add a synonym/meaning/reading note, and 'W' to wrap up. Try '?' for more hotkey info.
+// @version       1.3.0
 // @include       *://www.wanikani.com/lesson/session*
 // @include       *://www.wanikani.com/review/session*
 // @grant         none
@@ -22,7 +22,7 @@
   var clickAudioButton = () => $('#option-audio').trigger('click');
 
   // Expand flashcard info until `selector` is visible, then call `cb`.
-  var withInfo = (selector, cb, clicked) => {
+  var withInfo_Review = function(selector, cb, _clicked) {
     if (infoLoaded()) {
       if ($(selector).is(':visible')) {
         cb();
@@ -31,22 +31,38 @@
         setTimeout(cb, 1);
       }
     } else if (canGetInfo()) {
-      if (!clicked) clickShowInfo();
-      setTimeout(() => withInfo(selector, cb, true), 100);
+      if (!_clicked) clickShowInfo();
+      setTimeout(() => withInfo_Review(selector, cb, true), 100);
+    }
+  }
+
+  // If `#lesson` is visible:
+  //     If `lessonTab` is given and visible, click that and call `cb`.
+  // Otherwise (quiz or review):
+  //     Expand flashcard info until `selector` is visible, then call `cb`.
+  var withInfo = (selector, lessonTab, cb) => {
+    if ($('#lesson').is(':visible')) {
+      if (lessonTab && $(lessonTab).is(':visible')) {
+        $(lessonTab).trigger('click');
+        cb();
+      }
+    } else {
+      withInfo_Review(selector, cb);
     }
   };
 
   // Expand info until `selector` is visible. Then click `button` and focus `input`.
-  var clickEdit = (selector, button, input) => {
-    withInfo(selector, () => {
+  // (In lesson mode, click `lessonTab`, then `button`, then focus `input`.)
+  var clickEdit = (selector, button, input, lessonTab) => {
+    withInfo(selector, lessonTab, () => {
       $(button).trigger('click');
       setTimeout(() => $(input).focus(), 50);
     });
   }
 
   var addSynonym = () => clickEdit('.user-synonyms', '.user-synonyms-add-btn', '.user-synonyms input');
-  var editMeaning = () => clickEdit('.note-meaning', '.note-meaning', '.note-meaning textarea');
-  var editReading = () => clickEdit('.note-reading', '.note-reading', '.note-meaning textarea');
+  var editMeaning = () => clickEdit('.note-meaning', '.note-meaning:visible', '.note-meaning textarea', '#supplement-nav li:contains("meaning")');
+  var editReading = () => clickEdit('.note-reading', '.note-reading:visible', '.note-reading textarea', '#supplement-nav li:contains("reading")');
 
   // Play the preferred or non-preferred audio file.
   // This will use the big audio button if it's available.
@@ -55,17 +71,14 @@
     if (preferred && canClickAudioButton()) {
       clickAudioButton();
     } else {
-      withInfo('.audio-player', () => {
+      withInfo('.audio-player', '#supplement-nav li:contains("reading")', () => {
         var audio = $('.audio-player>audio').filter((i, e) => $(e).is('.preferred') === preferred)[0];
         if (audio) audio.play();
       });
     }
   };
 
-  $(document).on('keydown.reviewScreen', (e) => {
-    // Don't fire hotkeys until the review UI is visible.
-    if (!$('#reviews').is(':visible')) return;
-
+  $(document).on('keydown.moreHotkeys', (e) => {
     // The main hotkeys.
     var shift = e.shiftKey;
     if (!$('input, textarea').is(':focus')) {
@@ -106,6 +119,14 @@
         $('#hotkeys').trigger('click');
         e.preventDefault();
       }
+    }
+  });
+
+  // Disable lesson screen `keyup` J handler. (It caused to audios to play when Shift+J is pressed: one on keydown, one on keyup.)
+  // Trick: WaniKani's handler is bound on `document`, so we bind ours on `html` and stop propagation to `document` from there.
+  $('html').on('keyup.moreHotkeys', (e) => {
+    if (e.key.toLowerCase() === 'j') {
+      e.stopPropagation();
     }
   });
 
